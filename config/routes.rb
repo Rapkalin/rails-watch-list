9dcd1cfa82086450af5d02b5b0f634d371849b5c
Raphael Kalinowski <Raphael@myminicom.com>
2021-08-13 12:23:30 +0200

    adding bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "lists#index"
+
+  resources :movies, only: [:index, :show] do
+  end
+
+  resources :lists, only: [:index, :show, :create, :new] do
+    resources :bookmarks, only: [:create, :new]
+  end
+
+  resources :bookmarks, only: [:destroy]
+
+  # get "/movies", to: "movies#index"
+  # get "/movies/:id", to: "movies#show", as: "movie"
+  # get "/lists", to: "lists#index"
+  # get "/lists/new", to: "lists#new"
+  # get "/lists/:id", to: "lists#show", as: "list"
+  # post "/lists", to: "lists#create"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# db/seeds.rb
+
+puts "Cleaning database..."
+Movie.destroy_all
+puts "Database cleaned"
+
+puts "Creatings movies..."
+wonder_woman = {title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9}
+the_shawshank_redemption = {title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7}
+titanic = {title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9}
+ocean_eight = {title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0}
+movies = [wonder_woman, the_shawshank_redemption, titanic, ocean_eight]
+
+movies.each_with_index do |movie, index|
+  Movie.create(movie)
+  puts "#{index + 1} - #{movie[:title]} has been created"
+end
+
+puts "You have created #{movies.length} movies"
+puts "You are good to go!"
